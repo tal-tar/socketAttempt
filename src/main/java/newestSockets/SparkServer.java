685f2ff9813d3cb38e3a270168440531d44535b6
tal-tar <tal.tarnopolsky2001@gmail.com>
2021-10-30 18:14:11 +0300

    unsecure

--- src/main/java/newestSockets/SparkServer.java
+++ src/main/java/newestSockets/SparkServer.java
@@ -14,15 +14,15 @@ public class SparkServer {
     public static void main(String[] args) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
         String absolutePath = System.getenv("OPENSHIFT_REPO_DIR");
         System.out.println(absolutePath);
-        String keyStorePath = "src/main/resources/KeyStore.jks";
-        String pass = "123456";
-
-        String trustStorePath = "src/main/resources/truststore.jks";
-
-        KeyStore keystore = KeyStore.getInstance("JKS");
-        keystore.load(new FileInputStream(keyStorePath), pass.toCharArray());
-
-        secure(keyStorePath, pass, trustStorePath, pass);
+//        String keyStorePath = "src/main/resources/KeyStore.jks";
+//        String pass = "123456";
+//
+//        String trustStorePath = "src/main/resources/truststore.jks";
+//
+//        KeyStore keystore = KeyStore.getInstance("JKS");
+//        keystore.load(new FileInputStream(keyStorePath), pass.toCharArray());
+//
+//        secure(keyStorePath, pass, trustStorePath, pass);
 //        port(getOCPort());
 //        webSocket("/socket", SparkSocketServer.class);
         staticFiles.location("web");
